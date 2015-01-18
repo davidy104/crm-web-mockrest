35072286ee61650f42bbe89e51576ae3ef82c6d2
davidy <david.yuan124@gmail.com>
2015-01-18 16:53:58 +1300

    test

--- src/main/java/nz/co/at/crm/web/rest/config/ResourceModule.java
+++ src/main/java/nz/co/at/crm/web/rest/config/ResourceModule.java
@@ -1,10 +1,13 @@
 package nz.co.at.crm.web.rest.config;
 
+import nz.co.at.crm.web.rest.resources.CrmWebResources;
+
 import com.google.inject.AbstractModule;
 
 public class ResourceModule extends AbstractModule {
 	
 	@Override
 	protected void configure() {
+		bind(CrmWebResources.class);
 	}
 }
